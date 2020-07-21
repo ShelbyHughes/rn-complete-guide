cbba88a4f4f0620aa09f125859b9804fa7324d8b
Shelby Hughes <shelbyhughes2019@gmail.com>
2020-07-21 10:02:13 -0500

    messages

---
+++ components/GoalInput.js
@@ -0,0 +1,36 @@
+import React, { useState} from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const GoalInput = props => {
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+      };
+
+    return (
+    <View style={StyleSheet.inputContainer}>
+        <TextInput placeholder="Course Goal"
+        style={StyleSheet.input}
+        onChangeText={goalInputHandler}
+        value={enteredGoal}
+        />
+        <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+    </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'center'
+      },
+      input: {
+        borderColor: 'black', 
+        borderWidth: 1, 
+        padding: 10, 
+        width: '80%' 
+      }
+})
+
+export default GoalInput;
